177f8c558d1ba6d660390ec3e23f46b914315af1
jameson512 <2867557054@qq.com>
2023-11-21 03:08:33 +0800

    add

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import time
 
 from flask import Flask, request, render_template, jsonify, send_file
 import os
+ROOT_DIR=os.getcwd().replace('\\', '/')
+os.environ['TTS_HOME']=ROOT_DIR
+os.environ['PATH'] = ROOT_DIR + ';' + os.environ['PATH']
 import glob
 import hashlib
 import torch
@@ -18,69 +21,90 @@ from TTS.api import TTS
 import queue
 import webbrowser
 
-ROOT_DIR=os.getcwd().replace('\\', '/')
 
-#logging.basicConfig(
-#            level=logging.INFO,
-#            filename=f'{ROOT_DIR}/tts.log',
-#            encoding="utf-8",
-#            filemode="a"
-#)
-#print(f'{ROOT_DIR}/tts.log')
-#logger = logging.getLogger('TTS')
 
-# 存放录制好的素材，5-15s的语音mp3
-VOICE_DIR = os.path.join(ROOT_DIR, 'static/voicelist').replace('\\', '/')
+
+# 存放录制好的素材，5-15s的语音 wav
+VOICE_DIR = os.path.join(ROOT_DIR, 'static/voicelist')
 # 存放经过tts转录后的wav文件
-TTS_DIR = os.path.join(ROOT_DIR, 'static/ttslist').replace('\\', '/')
-os.environ['PATH'] = ROOT_DIR + ';' + os.environ['PATH']
+TTS_DIR = os.path.join(ROOT_DIR, 'static/ttslist')
+
+# 临时目录
+TMP_DIR=os.path.join(ROOT_DIR,'static/tmp')
+# 声音转声音 模型是否存在
+VOICE_MODEL_EXITS=True if os.path.exists(f"{ROOT_DIR}/tts/voice_conversion_models--multilingual--vctk--freevc24/model.pth") else False
+
 
 if not os.path.exists(VOICE_DIR):
     os.makedirs(VOICE_DIR)
 if not os.path.exists(TTS_DIR):
     os.makedirs(TTS_DIR)
+if not os.path.exists(TMP_DIR):
+    os.makedirs(TMP_DIR)
 
 q=queue.Queue(maxsize=100)
+q_sts=queue.Queue(maxsize=100)
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-globa_result={}
+global_tts_result={}
+global_sts_result={}
 
-# 合成线程
+# tts 合成线程
 def ttsloop():
-    global globa_result
-    
-    tts = TTS(
-            model_path=f"{ROOT_DIR}/models/tts_models--multilingual--multi-dataset--xtts_v2",
-            config_path=f"{ROOT_DIR}/models/tts_models--multilingual--multi-dataset--xtts_v2/config.json").to(device)
+    global global_tts_result    
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2"
+            #model_path=f"{ROOT_DIR}/models/tts_models--multilingual--multi-dataset--xtts_v2",
+            #config_path=f"{ROOT_DIR}/models/tts_models--multilingual--multi-dataset--xtts_v2/config.json"
+            ).to(device)
     while True:
         try:
             obj=q.get(block=True,timeout=1)
-            app.logger.info(f"开始合成，{obj=}")
+            app.logger.info(f"[tts][ttsloop]开始合成，{obj=}")
             try:
                 tts.tts_to_file(text=obj['text'], speaker_wav=os.path.join(VOICE_DIR, obj['voice']), language=obj['language'], file_path=f"{TTS_DIR}/{obj['filename']}")
-                globa_result[obj['filename']]=1
-                app.logger.info(f"合成结束{obj=}")
+                global_tts_result[obj['filename']]=1
+                app.logger.info(f"[tts][ttsloop]合成结束{obj=}")
             except Exception as e:
-                print(f"合成失败:{str(e)}")
-                app.logger.error(f"合成失败:{str(e)}")
-                globa_result[obj['filename']]=str(e)           
+                app.logger.error(f"[tts][ttsloop]合成失败:{str(e)}")
+                global_tts_result[obj['filename']]=str(e)           
         except Exception as e:
             pass
-            # print(e)
+
+
+# s t s 线程
+def stsloop():
+    global global_sts_result
+    
+    tts = TTS(model_name='voice_conversion_models/multilingual/vctk/freevc24').to(device)
+    while True:
+        try:
+            obj=q_sts.get(block=True,timeout=1)
+            app.logger.info(f"[sts][stsloop]开始转换声音，{obj=}")
+            try:
+                tts.voice_conversion_to_file(source_wav=TMP_DIR+"/"+obj['filename'], target_wav=os.path.join(VOICE_DIR, obj['voice']), file_path=f"{TTS_DIR}/{obj['filename']}")
+                global_sts_result[obj['filename']]=1
+                app.logger.info(f"[sts][stsloop]合成结束{obj=}")
+            except Exception as e:
+                app.logger.error(f"[sts][stsloop]转换声音失败:{str(e)}")
+                global_sts_result[obj['filename']]=str(e)           
+        except Exception as e:
+            pass
 
 
 # 实际启动tts合成的函数
 def create_tts(*,text,voice,language,filename):
-    global globa_result
+    global global_tts_result
     if os.path.exists(f"{TTS_DIR}/{filename}") and os.path.getsize(f"{TTS_DIR}/{filename}") > 0:
-        app.logger.info(f"{filename}已存在，直接返回")
-        globa_result[filename]=1
+        app.logger.info(f"[tts][create_ts]{filename}已存在，直接返回")
+        global_tts_result[filename]=1
         return {"code": 0, "filename": f"{TTS_DIR}/{filename}", 'name': filename}
     try:
-        app.logger.info(f"{text}压入队列，准备合成")
+        app.logger.info(f"[tts][create_ts]{text}压入队列，准备合成")
         q.put({"voice": voice, "text": text, "language": language, "filename": filename})
     except Exception as e:
         print(e)
+        app.logger.error(f"[tts][create_ts]合成出错，{str(e)}")
         return {"code": 1, "msg": str(e)}
     return None
 
@@ -104,18 +128,14 @@ file_handler.setFormatter(formatter)
 # 将文件处理器添加到日志记录器中
 app.logger.addHandler(file_handler)
 
-# 在应用程序中记录一条日志
-app.logger.info('这是一条日志信息')
-
 @app.route('/static/<path:filename>')
 def static_files(filename):
     return send_from_directory(app.config['STATIC_FOLDER'], filename)
 
 @app.route('/')
 def index():
-    info=f"host={request.host}"
-    app.logger.info(f"{info=}")
-    return render_template("index.html",info=info)
+    voice_model="yes" if VOICE_MODEL_EXITS else "no"
+    return render_template("index.html",voice_model=voice_model,root_dir=ROOT_DIR)
 
 # 上传音频
 @app.route('/upload', methods=['POST'])
@@ -123,47 +143,36 @@ def upload():
     try:
         # 获取上传的文件
         audio_file = request.files['audio']
-
+        save_dir=request.form.get("save_dir")
+        save_dir=VOICE_DIR if not save_dir else f"{ROOT_DIR}/static/{save_dir}"
+        app.logger.info(f"[upload]上传文件{audio_file.filename=},{save_dir=}")
         # 检查文件是否存在且是 WAV 格式
-        if audio_file and audio_file.filename.endswith('.mp3'):
+        if audio_file and audio_file.filename.endswith('.wav'):
             # 保存文件到服务器指定目录
-            name=f"{os.path.basename(audio_file.filename)}"
-            if os.path.exists(f"{VOICE_DIR}/{name}"):
+            name=f"{os.path.basename(audio_file.filename.replace(' ',''))}"
+            if os.path.exists(f"{save_dir}/{name}"):
                 name = f'{datetime.datetime.now().strftime("%m%d-%H%M%S")}-{name}'
-            audio_file.save(f"{VOICE_DIR}/{name}")
+            audio_file.save(f"{save_dir}/{name}")
             # 返回成功的响应
-            return {'code': 0, 'message': '上传成功',"data":name}
+            return {'code': 0, 'msg': '上传成功',"data":name}
         else:
             # 返回错误的响应
-            return {'code': 1, 'message': '上传的文件不是 mp3 格式'}
+            return {'code': 1, 'msg': '上传的文件不是 wav 格式'}
     except Exception as e:
-        print(f'上传错误: {e}')
-        return {'code': 2, 'message': '上传失败'}
+        app.logger.error(f'[upload]上传错误: {e}')
+        return {'code': 2, 'msg': '上传失败'}
 
-# 从 voicelist 目录获取可用的mp3声音列表
+# 从 voicelist 目录获取可用的 wav 声音列表
 @app.route('/init')
 def init():
-    mp3s = glob.glob(f"{VOICE_DIR}/*.mp3")
+    wavs = glob.glob(f"{VOICE_DIR}/*.wav")
     result = []
-    for it in mp3s:
+    for it in wavs:
         if os.path.getsize(it) > 0:
             result.append(os.path.basename(it))
     return jsonify(result)
 
-# 根据名称 ?name=wav文件名，返回音频的二进制数据流，用于前端shiy  /ttslist?name=文件名请求
-@app.route('/ttslist')
-def getfile():
-    filename=request.args.get("name")
-    if os.path.exists(f"{TTS_DIR}/{filename}"):
-        return send_file(f"{TTS_DIR}/{filename}", as_attachment=True)
-    return None
-# 根据名称 ?name=mp3文件名，返回音频的二进制数据流，用于前端shiy  /ttslist?name=文件名请求
-@app.route('/voicelist')
-def getmp3():
-    filename=request.args.get("name")
-    if os.path.exists(f"{VOICE_DIR}/{filename}"):
-        return send_file(f"{VOICE_DIR}/{filename}", as_attachment=True)
-    return None
+
 
 # 判断是否符合字幕格式，如果是，则直接返回
 # 从字幕文件获取格式化后的字幕信息
@@ -228,14 +237,12 @@ def get_subtitle_from_srt(txt):
 # 请求端根据需要自行选择使用哪个
 @app.route('/tts', methods=['GET', 'POST'])
 def tts():
-    global globa_result
+    global global_tts_result
     # 原始字符串
     text=request.form.get("text").strip()
     voice=request.form.get("voice")
     language=request.form.get("language")
-
-    app.logger.info(f"start->{text=}\n{voice=}\n{language=}\n\n")
-
+    app.logger.info(f"[tts][tts]接收到 {text=}\n{voice=},{language=}\n")
 
     if re.match(r'^[~`!@#$%^&*()_+=,./;\':\[\]{}<>?\\|"，。？；‘：“”’｛【】｝！·￥、\s\n\r -]*$',text):
         return jsonify({"code":1,"msg":"不存在有效text，无法合成"})
@@ -244,26 +251,28 @@ def tts():
 
     # 判断是否是srt
     text_list=get_subtitle_from_srt(text)
-    app.logger.info(f"{text_list=}")
+    app.logger.info(f"[tts][tts]{text_list=}")
     is_srt=False
     # 不是srt格式
     if text_list is None:
         text_list=[{"text":text}]
+        app.logger.info(f"[tts][tts]不是srt格式")
     else:
         # 是字幕
         is_srt=True
+        app.logger.info(f"[tts][tts]是srt格式")
     num=0
     response_json={}
     while num<len(text_list):
         t=text_list[num]
-        #换行符
-        t['text']=t['text'].replace("\n",'.')
+        #换行符改成 .
+        t['text']=t['text'].replace("\n",' . ')
         md5_hash = hashlib.md5()
         md5_hash.update(f"{t['text']}-{voice}-{language}".encode('utf-8'))
         filename = md5_hash.hexdigest()+".wav"
-        app.logger.info(f"{filename=}")
+        app.logger.info(f"[tts][tts]{filename=}")
         # 合成语音
-        rs=create_tts(text= t['text'],voice=voice,language=language,filename=filename)
+        rs=create_tts(text=t['text'],voice=voice,language=language,filename=filename)
         # 已有结果或错误，直接返回
         if rs is not None :
             if not is_srt:
@@ -274,35 +283,85 @@ def tts():
 
         # 循环等待 最多7200s
         time_tmp=0
-        while filename not in globa_result:        
-            time.sleep(10)
-            time_tmp+=10
-            app.logger.info(f"{time_tmp=}，{filename=}")
+        while filename not in global_tts_result:        
+            time.sleep(3)
+            time_tmp+=3
+            if time_tmp % 30==0:
+                app.logger.info(f"[tts][tts]{time_tmp=}，{filename=}")
 
         # 当前行已完成合成
-        if globa_result[filename]!=1:
-            msg={"code":1,"msg":globa_result[filename]}
+        if global_tts_result[filename]!=1:
+            msg={"code":1,"msg":global_tts_result[filename]}
         else:
             msg={"code":0,"filename":f"{TTS_DIR}/{filename}",'name':filename}
-        globa_result.pop(filename)
+        app.logger.info(f"[tts][tts]当前结果,{filename=},{msg=}")
+        global_tts_result.pop(filename)
         if not is_srt:
             response_json=msg            
             break
         
         text_list[num]['result']=msg
-        app.logger.info(f"{num=}")
+        app.logger.info(f"[tts][tts]{num=}")
         num+=1
     # 不是字幕则返回
-    app.logger.info(f"{response_json=}")
     if not is_srt:
+        app.logger.info(f"[tts][tts] 不是srt字幕，最终结果 {response_json=}")
         return jsonify(response_json)
     # 继续处理字幕
-    # filename=""
+    
     filename,errors=merge_audio_segments(text_list)
-    app.logger.info(f"{filename=},{errors=}")
+    app.logger.info(f"[tts][tts]是srt字幕格式，{filename=},{errors=}")
     if filename and os.path.exists(filename) and os.path.getsize(filename)>0:
-        return jsonify({"code":0,"filename":filename,"name":os.path.basename(filename),"msg":errors})
-    return jsonify({"code":1,"msg":f"合成出错了:{filename=},{errors=}"})
+        res={"code":0,"filename":filename,"name":os.path.basename(filename),"msg":errors}
+    else:
+        res={"code":1,"msg":f"合成出错了:{filename=},{errors=}"}
+    app.logger.info(f"[tts][tts]最终合成结果:{res=}")
+    return jsonify(res)
+
+
+#s to s wav->wav
+@app.route('/sts', methods=['GET', 'POST'])
+def sts():
+    global global_sts_result
+    try:
+        # 保存文件到服务器指定目录
+        # 目标
+        voice=request.form.get("voice")
+        filename=request.form.get("name")
+        app.logger.info(f"[sts][sts]获取到 sts {voice=},{filename=}\n")
+
+        if not voice:
+            return jsonify({"code":1,"msg":"voice 参数不可缺少"})
+
+        obj={"filename":filename,"voice":voice}
+        # 压入队列，准备转换语音
+        app.logger.info(f"[sts][sts]压入 sts队列，准确转换")
+        q_sts.put(obj)
+        # 已有结果或错误，直接返回
+        # 循环等待 最多7200s
+        time_tmp=0
+        while filename not in global_sts_result:        
+            time.sleep(3)
+            time_tmp+=3
+            if time_tmp%30==0:
+                app.logger.info(f"{time_tmp=}，{filename=}")
+        
+        # 当前行已完成合成
+        if global_sts_result[filename]!=1:
+            msg={"code":1,"msg":global_sts_result[filename]}
+            app.logger.error(f"[sts][sts]转换失败，{msg=}")
+        else:
+            msg={"code":0,"filename":f"{TTS_DIR}/{filename}",'name':filename}
+            app.logger.info(f"[sts][sts]转换成功，{msg=}")            
+        global_sts_result.pop(filename)
+        return jsonify(msg)
+    except Exception as e:
+        app.logger.error(f"[sts][sts]转换失败:{str(e)}")
+        return jsonify({'code': 2, 'msg': f'声音转声音失败:{str(e)}'})
+        
+
+
+
 
 # join all short audio to one ,eg name.mp4  name.mp4.wav
 def merge_audio_segments(text_list):
@@ -356,9 +415,13 @@ def openweb():
 
 if __name__ == '__main__':
     try:        
-        print("地址: http://127.0.0.1:9988")
+        print("本地web地址: http://127.0.0.1:9988")
+        app.logger.info("本地web地址: http://127.0.0.1:9988")
         threading.Thread(target=ttsloop).start()
+        if VOICE_MODEL_EXITS:
+            threading.Thread(target=stsloop).start()
         threading.Thread(target=openweb).start()
         app.run(port=9988)
     except Exception as e:
         print("执行出错:"+str(e))
+        app.logger.error(f"[app]启动出错:{str(e)}")
--- tts.py
+++ tts.py
@@ -1,24 +1,33 @@
 import torch
 from TTS.api import TTS
-
+import os
+os.environ['TTS_HOME']=os.getcwd()
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # List available 🐸TTS models
-print(TTS().list_models())
+#print(TTS().list_models())
 
 # exit()
 # Init TTS
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+#tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 # Run TTS
 # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
 # Text to speech list of amplitude values as output
-wav = tts.tts(text="Text to speech list of amplitude values as output!", speaker_wav="sx.mp3", language="en")
+#wav = tts.tts(text="Text to speech list of amplitude values as output!", speaker_wav="sx.mp3", language="en")
 # Text to speech to a file
 #['en', 'es', 'fr', 'de', 'it', 'pt', 'pl', 'tr', 'ru', 'nl', 'cs', 'ar', 'zh-cn', 'hu', 'ko', 'ja']
-tts = TTS(
-    model_path=f"./models/tts_models--multilingual--multi-dataset--xtts_v2",config_path=f"./models/tts_models--multilingual--multi-dataset--xtts_v2/config.json").to(device)
-tts.tts_to_file(text="我是中国人, 你是哪里人呢，我的朋友!", speaker_wav="cn1.mp3", language="zh-cn", file_path="output1.wav")
+#tts = TTS(model_path=f"./models/tts_models--multilingual--multi-dataset--xtts_v2",config_path=f"./models/tts_models--multilingual--multi-dataset--xtts_v2/config.json").to(device)
+tts = TTS(model_name='voice_conversion_models/multilingual/vctk/freevc24').to(device)
+#tts = TTS(model_name='voice_conversion_models/multilingual/vctk/freevc24',model_path=f"E:/python/tts/models/voice_conversion_models--multilingual--vctk--freevc24",config_path="E:/python/tts/models/voice_conversion_models--multilingual--vctk--freevc24/config.json").to(device)
+#tts.tts_to_file(text="我是中国人, 你是哪里人呢，我的朋友!", speaker_wav="cn1.mp3", language="zh-cn", file_path="output1.wav")
+print(f"=======")
+#tts.voice_conversion_to_file(source_wav="cn1.wav", target_wav="sx1.wav", file_path="output.wav")
+
+#tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2"
+            #model_path=f"{ROOT_DIR}/models/tts_models--multilingual--multi-dataset--xtts_v2",
+            #config_path=f"{ROOT_DIR}/models/tts_models--multilingual--multi-dataset--xtts_v2/config.json"
+#            ).to(device)
 
 # pip install torch==2.0.1+cu118 torchvision=0.16.1+cu118 torchaudio===0.13.0 -f https://download.pytorch.org/whl/torch_stable.html#
