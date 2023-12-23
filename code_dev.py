da2351233a8cbf600ce1f447d0adf0ac66687d0b
jameson512 <2867557054@qq.com>
2023-12-23 23:46:25 +0800

    multi language

--- code_dev.py
+++ code_dev.py
@@ -14,10 +14,13 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 
-#tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
-#tts.tts_to_file(text='我是中国人', speaker_wav='./cn1.wav',language='zh', file_path='ha.wav')
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
 tts = TTS(model_name='voice_conversion_models/multilingual/vctk/freevc24').to(device)
-tts.voice_conversion_to_file(source_wav="./cn1.wav",
-                                         target_wav="./sx1.wav", # voice file
-                                         file_path="./out.wav")
+
+# test
+# tts.tts_to_file(text='我是中国人', speaker_wav='./cn1.wav',language='zh', file_path='ha.wav')
+
+#target_wav is voice file 
+# tts.voice_conversion_to_file(source_wav="./cn1.wav", target_wav="./sx1.wav", file_path="./out.wav")
 
